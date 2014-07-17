ec8892e6f3144675ea7a0aeeea1bb2e0d0cdefbb
SRozick <steven.rozick@teachafish.org>
2014-07-17 15:46:32 -0700

    Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root to:"pages#home"
+  # this command sends to pages controller, home action
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
